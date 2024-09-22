ebb6ec8cc51a7ea35b13e472f9d3097805e58586
unknown <matifandy@gmail.com>
2024-09-22 18:18:34 -0300

    feat. file upload compnent

--- src/app/components/FileUpload/FileUpload.tsx
+++ src/app/components/FileUpload/FileUpload.tsx
@@ -1,19 +1,92 @@
-// components/FileUpload.js
+"use client";
 
 import React from "react";
+import { ChangeEvent, Dispatch, useState } from "react";
 import styles from "./FileUpload.module.css";
 
-const FileUpload = () => {
+const FileUpload = ({ setFile }: { setFile: Dispatch<File> }) => {
+  const [isDragging, setIsDragging] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const [successMessage, setSuccessMessage] = useState<string | null>(null);
+
+  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      if (!isValidFileType(file)) {
+        setError("Unsupported file type. Please upload an image or PDF.");
+        return;
+      }
+      setFile(file);
+      setSuccessMessage("File uploaded successfully!");
+      setError(null);
+    }
+  };
+
+  const handleDragOver = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    setIsDragging(true);
+  };
+
+  const handleDragLeave = () => {
+    setIsDragging(false);
+  };
+
+  const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    setIsDragging(false);
+    const file = event.dataTransfer.files?.[0];
+    if (event.dataTransfer.files.length > 1) {
+      setError("Only one file can be uploaded at a time.");
+      return;
+    }
+    if (file) {
+      if (!isValidFileType(file)) {
+        setError("Unsupported file type. Please upload an image.");
+        return;
+      }
+      setFile(file);
+      setSuccessMessage("File uploaded successfully!");
+      setError(null);
+    }
+  };
+
+  const isValidFileType = (file: File) => {
+    const validTypes = [
+      "image/png",
+      "image/jpg",
+      "image/jpeg",
+      "image/webp",
+      "image/bmp",
+      "image/heic",
+      "image/tif",
+      "image/tiff",
+      "image/gif",
+    ];
+    return validTypes.includes(file.type);
+  };
+
   return (
-    <div className={styles.input}>
-      <input
-        className={styles.inputFile}
-        type="file"
-        aria-label="upload"
-        name="file"
-        accept=".png, .jpg, .jpeg, .pdf, .webp, .bmp, .heic, .tif, .tiff, .gif"
-        id="fileInput"
-      />
+    <div>
+      <div
+        role="region"
+        tabIndex={0}
+        className={`${styles.input} ${isDragging ? styles.dragging : ""}`}
+        onDragOver={handleDragOver}
+        onDragLeave={handleDragLeave}
+        onDrop={handleDrop}
+      >
+        <input
+          className={styles.inputFile}
+          type="file"
+          aria-label="upload"
+          name="file"
+          accept=".png, .jpg, .jpeg, .pdf, .webp, .bmp, .heic, .tif, .tiff, .gif"
+          id="fileInput"
+          onChange={handleFileChange}
+        />
+      </div>
+      {error && <p className={styles.error}>{error}</p>}
+      {successMessage && <p className={styles.success}>{successMessage}</p>}
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+"use client";
+
+import { useState } from "react";
 import FileUpload from "./components/FileUpload/FileUpload";
 import styles from "./page.module.css";
+import Image from "next/image";
 
 export default function Home() {
+  const [file, setFile] = useState<File>();
+
+  console.log(file);
+
   return (
     <main className={styles.main}>
       <h1 className={styles.title}>CleanPic</h1>
@@ -10,7 +18,32 @@ export default function Home() {
         metadata from photos and images. This helps to protect your privacy and
         reduce the image file size.
       </p>
-      <FileUpload />
+      <FileUpload setFile={setFile} />
+      {file && (
+        <div className={styles.imageContainer}>
+          <Image
+            src={URL.createObjectURL(file)}
+            alt="CleanPic"
+            width={300}
+            height={200}
+            objectFit="contain"
+          />
+          <div className={styles.fileDetails}>
+            <p>File: {file?.name}</p>
+            <p>Size: {file?.size}</p>
+            <p>Type: {file?.type}</p>
+            <p>Last Modified: {file?.lastModified}</p>
+          </div>
+          <button>Download Cleared Image</button>
+        </div>
+      )}
+      <div className={styles.howtouse}>
+        <h2>How to remove EXIF metadata from your photos and images</h2>
+        <ol>
+          <li>Upload a photo or image file</li>
+          <li>Download cleared photo</li>
+        </ol>
+      </div>
     </main>
   );
 }
